78f60475321978f53cf466f095783423412370f5
ahonculada <achonculada@gmail.com>
2022-04-14 04:38:08 -0400

    html frontend for displaying ascii using jinja2. route for form and for individual animals based on path parameter.

--- site/main.py
+++ site/main.py
@@ -1,7 +1,9 @@
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="../templates/")
 
 class AnimalName(str, Enum):
     fish = 'fish'
@@ -11,20 +13,43 @@ class AnimalName(str, Enum):
 async def root():
     return { "message": "Hello, Girl!"}
 
-@app.get('/{animal_name}')
-async def get_animal(animal_name: AnimalName):
+@app.get('/animal/{animal_name}')
+async def get_animal(request: Request, animal_name: AnimalName):
     if animal_name == AnimalName.fish:
         with open('../ascii/fish.txt') as data:
             image = ''
             for line in data:
                 image += line
-            #print(image)
 
     elif animal_name == AnimalName.cat:
         with open('../ascii/cat.txt') as data:
             image = ''
             for line in data:
                 image += line
-    return {'animal_name': animal_name,
-            'message': image 
-            }
+    #return {'animal_name': animal_name,
+    #        'message': image
+    #        }
+    result = image
+    return templates.TemplateResponse('animal.html', context={'request': request, 'result': result})
+
+@app.get('/animal')
+async def form_post(request: Request):
+    result = "Which animal would you like to see?"
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+@app.post('/animal')
+async def form_post(request: Request, animal: str = Form(...)):
+    if animal == AnimalName.fish:
+        with open('../ascii/fish.txt') as data:
+            image = ''
+            for line in data:
+                image += line
+
+    elif animal == AnimalName.cat:
+        with open('../ascii/cat.txt') as data:
+            image = ''
+            for line in data:
+                image += line
+    result = image
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
