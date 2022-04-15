0e2e33e17c43b97031a2985135a0e30d889b5cab
Alex D <elxaitisaley@gmail.com>
2022-04-15 19:42:10 -0400

    first working jinja template in the cloud, alongside secrets gitignored

---
+++ site/database.py
@@ -0,0 +1,13 @@
+import pymongo
+
+with open('../secrets.txt') as data:
+    password = ''.join(data.readlines())[:-1]
+
+client = pymongo.MongoClient(f"mongodb+srv://miyazaki:{ password }@miyazaki-cluster-1.qhpgx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+
+def getAscii(animal_name: str) -> str:
+    mydb = client['Miyazaki-db']
+    animals = mydb['animals']
+    animal = animals.find_one({'name': f'{animal_name}'})
+    return animal['ascii']
--- site/main.py
+++ site/main.py
@@ -1,13 +1,19 @@
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+from database import getAscii
 
 app = FastAPI()
+templates = Jinja2Templates(directory="../templates/")
 
 @app.get('/')
-async def root():
-    return { "message": "Hello, Girl!"}
+async def root(request: Request):
+    # result = getAscii(animal_name='cat')
+    # return {"cat": result}
+    return templates.TemplateResponse('landing.html', context={'request': request, 'result': None})
 
 if __name__ == '__main__':
     uvicorn.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 8080)))
